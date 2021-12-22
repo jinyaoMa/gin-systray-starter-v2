c656571bbfcde89ecaa10effc7adaa8f6d0f5596
jinyaoMa <jinyao.ma@outlook.com>
2021-12-22 13:29:08 -0500

    add air

--- main.go
+++ main.go
@@ -1,12 +1,22 @@
 package main
 
 import (
+	"App/routers"
 	"App/tray"
+	"flag"
 )
 
-func main() {
-	// config := GetConfig()
+var hasTray = flag.Int("t", 1, "set to enable system tray")
 
-	// tray.SetConfig(config.Tray)
-	tray.Start()
+func main() {
+	flag.Parse()
+
+	if *hasTray == 1 {
+		// config := GetConfig()
+
+		// tray.SetConfig(config.Tray)
+		tray.Start()
+	} else {
+		routers.Start(true, true)
+	}
 }
